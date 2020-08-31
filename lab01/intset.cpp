605bcc94c464d258d9cb0460f847a13988b1a1a4
ozzhaas <kellenhaas@outlook.com>
2020-08-30 22:31:04 -0400

    8.30 10:30pm

--- lab01/intset.cpp
+++ lab01/intset.cpp
@@ -1,81 +1,149 @@
+/*Kellen Haas
+  CPSC 2121
+  Lab 1
+  8/30/20
+*/
+
+
 #include <iostream>
 #include <assert.h>
 #include "intset.h"
 
 using namespace std;
 
-Intset::Intset()
-{
-  size = 0;
-  allocated = 10;  /* start with a 10-element array */
-  A = new int[10];
+//Constructor to create the Intset object
+Intset::Intset() {
+    //Initialize the head to null
+    head = nullptr;
 }
 
-Intset::~Intset()
-{
-  delete[] A;
+//Destructor to remove the Intset Object and the list entirely
+Intset::~Intset() {
+    //Make a pointer to the head
+    ListNode *headPtr = head;
+
+    //Loop until the end of the list...
+    while (headPtr != nullptr) {
+
+        /*Make a ListNode pointer object
+          to store the garbage nodes*/
+        ListNode *junkNode = headPtr;
+
+        //Move the head to the next node
+        headPtr = headPtr->next;
+
+        //Delete the garbage node
+        delete junkNode;
+    }
 }
 
 /* Return true if key is in the set */
-bool Intset::find(int key)
-{
-  int low = 0, high = size-1, mid;
+bool Intset::find(int key) {
+    //Create a temp ListNode pointer and assign it the head's value
+    ListNode *tmp = head;
 
-  /* Binary search for key... */
-  while (low <= high) {
-    mid = (low + high) / 2;
-    if (key == A[mid]) return true;
-    if (key > A[mid]) low = mid+1;
-    else high = mid-1;
-  }
-  return false;
+    /* Linear search for key...
+       if the head is null or the head's value
+       is the same as the key return false*/
+    if (head == nullptr || head->num > key) {
+        return false;
+    }
+
+    //While the tmp is not null keep looping
+    while (tmp != nullptr) {
+        //If tmp's value is equal to key return true
+        if (tmp->num == key) {
+            return true;
+        }
+        //Else move onto the next node
+        else {
+            tmp = tmp->next;
+        }
+    }
+    return false;
 }
 
 /* Inserts a new key.  It is an error if key is already in the set. */
-void Intset::insert(int key)
-{
-  int i;
+void Intset::insert(int key) {
+    //Create a ListNode pointer to the head and the previous node
+    ListNode *headPtr;
+    ListNode *prevNodePtr;
 
-  assert (!find(key));
+    assert (!find(key));
 
-  /* Enlarge (by 2x) the underlying array if it isn't large enough... */
-  if (size == allocated) {
-    allocated *= 2;
-    int *newA = new int[allocated];
-    for (i=0; i<size; i++) newA[i] = A[i];
-    delete[] A;
-    A = newA;
-  }
+    /*If the head is null or head's value is greater than
+      or equal to the key create the new node to insert at
+      the front of the list*/
+    if (head == nullptr || head->num >= key) {
+        head = new ListNode(key, head);
+    }
+    /*Else point the previous node pointer to the head
+      and move the headPtr to the next node */
+    else {
+        prevNodePtr = head;
+        headPtr = head->next;
 
-  /* Shift up elements in array to make room for new element... */
-  size++;
-  i = size-1;
-  while (i>0 && A[i-1]>key) {
-    A[i] = A[i-1];
-    i--;
-  }
-  A[i] = key;
+        /*While the headPtr isn't null and head's value is less
+          than the key, reassign the previous node pointer to
+          the headPtr and move the headPtr to the next node*/
+        while (headPtr != nullptr && headPtr->num < key) {
+            prevNodePtr = headPtr;
+            headPtr = headPtr->next;
+        }
+        /*Create a new node after the previous node with key
+          and headPtr passed into the constructor*/
+        prevNodePtr->next = new ListNode(key, headPtr);
+    }
 }
 
 /* Removes a key.  It is an error if key isn't in the set */
-void Intset::remove(int key)
-{
-  int i;
+void Intset::remove(int key) {
+    assert (find(key));
 
-  assert (find(key));
+    //Create a ListNode pointer to the head and the previous node
+    ListNode *prevNodePtr, *headPtr;
 
-  /* Shift down elements in array to plug gap left by deleted element... */
-  for (i=size-1; key!=A[i]; i--);
-  while (i<size-1) {
-    A[i] = A[i+1];
-    i++;
-  }
-  size--;
+    //If the head is null then return
+    if (!head) {return;}
+
+    /*If the head's value is equal to the key
+      assign headPtr to the head and then move
+      the head to the next node and then delete
+      the headPtr*/
+    if (head->num == key) {
+        headPtr = head;
+        head = head->next;
+        delete headPtr;
+    }
+
+    /*Else assign headPtr to head and then start loop to assign
+      the previous node pointer to headPtr and move the headPtr
+      on to the next node*/
+    else {
+        headPtr = head;
+
+        while(headPtr != nullptr && headPtr->num != key) {
+            prevNodePtr = headPtr;
+            headPtr = headPtr->next;
+        }
+
+        /*If the headPtr isn't null assign the node after
+          previous node to the node after headPtr, then
+          delete headPtr*/
+        if (headPtr) {
+            prevNodePtr->next = headPtr->next;
+            delete headPtr;
+        }
+    }
 }
 
-void Intset::print(void)
-{
-  int i;
-  for (i=0; i<size; i++)
-    cout << A[i] << "\n";
+/*Function to display the entire list by creating a pointer
+  to the head and incrementing it through the list*/
+void Intset::print(void) {
+    ListNode *headPtr = head;
+    while (headPtr) {
+        cout << headPtr->num << " ";
+        headPtr = headPtr->next;
+    }
+    cout << "\n";
 }
--- lab01/intset.h
+++ lab01/intset.h
@@ -1,19 +1,37 @@
+/*Kellen Haas
+  CPSC 2121
+  Lab 1
+  8/30/20
+*/
+
+
 #ifndef INTSET_H /* Prevent multiple inclusion... */
 #define INTSET_H
 
+
+
 class Intset {
 
- private: 
-  int size, allocated;
-  int *A;
+    private:
+        //Create a ListNode structure
+        struct ListNode {
+            int num;
+            ListNode *next;
+            ListNode(int num1, ListNode *next1 = nullptr) {
+                num = num1;
+                next = next1;
+            }
+        };
+        //Create the head of the list
+        ListNode *head;
 
- public:
-  Intset();
-  ~Intset();
-  bool find(int key);
-  void insert(int key);
-  void remove(int key);
-  void print(void);
-};
+    public:
+        Intset();
+        ~Intset();
+        bool find(int key);
+        void insert(int key);
+        void remove(int key);
+        void print(void);
+    };
 
 #endif
