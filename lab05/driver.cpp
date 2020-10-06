77730f3ff0a2246a2119855a70c76ffece36818b
Kellen Haas <kellenhaas@outlook.com>
2020-10-05 20:22:34 -0400

    10.5 8:22pm

---
+++ lab05/driver.cpp
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+using namespace std;
+
+
+
+
+
+
+
+string readInput(const char *filename) {
+
+
+
+
+
+
+
+
+
+}
+
+
+
+int main () {
+    ifstream input;
+    input.open("cuts.txt");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
