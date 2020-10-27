24d7e0a568e8982c9800a7b4c34801c069426934
Kellen Haas <kellenhaas@outlook.com>
2020-10-27 00:02:13 -0400

    10.27 12:02am

--- lab07/n_queens.cpp
+++ lab07/n_queens.cpp
@@ -7,9 +7,6 @@
 
 
 #include <iostream>
-#include <fstream>
-#include <sstream>
-#include <vector>
 #include <iomanip>
 
 using namespace std;
@@ -17,12 +14,32 @@ using namespace std;
 int N, half;
 int **board;
 int count;
-
+bool oddBall = false;
+bool even = false;
 
 
 bool check_pos(int r, int c) {
     int row = r;
     int col = c;
+
+    int leftdiag[N];
+    int rightdiag[N];
+    int column[N];
+
+
+/*******************Second Solution...May Be Faster?*******************/
+    // for (int i = 0; i < N; i++) {
+    //     if ((leftdiag[i - c + N - 1] != 1 && rightdiag[i + c] != 1) && column[i] != 1) {
+    //         board[i][c] = 1;
+    //         leftdiag[i - c + N - 1] = rightdiag[i + c] = column[i] = 1;
+    //         if (check_pos(r, c + 1)) {return true;}
+    //         board[i][c] = 0;
+    //         leftdiag[i - c + N - 1] = rightdiag[i + c] = column[i] = 0;
+    //     }
+    // }
+    // return false;
+
+
 
     for (int i = r - 1; i >= 0; i--) {
         if (board[i][c]) {
@@ -36,7 +53,7 @@ bool check_pos(int r, int c) {
         }
     }
 
-    for (row = r - 1, col = c + 1; row >= 0 && col < N; row--, col++) {
+    for (row = r - 1, col = c + 1; col >= 0 && r < N; row++, col--) {
         if (board[row][col]) {
             return false;
         }
@@ -47,52 +64,57 @@ bool check_pos(int r, int c) {
 
 
 void check_row(int r) {
+
     if (r == N) {
-        count++;
+        count += 2;
+        if (even) {
+            count - 1;
+        }
         return;
     }
-    for (int i = 0; i < N; i++) {
-        if (check_pos(r, i)) {
-            board[r][i] = true;
-            check_row(r + 1);
-            board[r][i] = false;
+    if (r != 0) {
+        for (int col = 0; col < N; col++) {
+            if (check_pos(r, col)) {
+                board[r][col] = true;
+                check_row(r + 1);
+                board[r][col] = false;
+            }
+        }
+    }
+    else if (N % 2 == 0) {
+        for (int col = 0; col < N/2; col++) {
+            if (check_pos(r, col)) {
+                board[r][col] = true;
+                check_row(r + 1);
+                board[r][col] = false;
+            }
+        }
+    }
+    else {
+        for (int col = 0; col < (N/2) + 1; col++) {
+            if (col == N/2) {
+                even = true;
+            }
+            if (check_pos(r, col)) {
+                board[r][col] = true;
+                check_row(r + 1);
+                board[r][col] = false;
+            }
         }
     }
 }
 
 
-
-
-
-
-/*Checks the number of command line arguments*/
-bool checkArgs(int argc){
-    if (argc != 2) {
-        cout << "No input file provided. Try again.\n";
-        return false;
-    }
-    return true;
-}
 
 int main(int argc, char* argv[]) {
 
     N = atoi(argv[1]);
-    bool oddBall = false;
 
-    half = N/2;
-    oddBall = N % 3;
-    if (oddBall) {
-        half = (N/2) + oddBall;
+    board = new int* [N];
+    for (int i = 0; i < N; i++) {
+        board[i] = new int[N];
     }
-
-
-    if (checkArgs(argc)) {
-        board = new int* [N];
-        for (int i = 0; i < N; i++) {
-            board[i] = new int[N];
-        }
-        check_row(0);
-    }
+    check_row(0);
 
 
     cout << "Total Number of Solutions: " << count << endl;
