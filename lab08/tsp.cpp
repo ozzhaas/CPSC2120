a0b71d5b30ec9b0382a512f35d8f1fdac11a068c
ozzhaas <kellenhaas@outlook.com>
2020-11-04 23:11:08 -0500

    11.4 11:11pm

--- lab08/tsp.cpp
+++ lab08/tsp.cpp
@@ -17,18 +17,20 @@
 
 using namespace std;
 
-
+/*Total Number of Cities*/
 const int N = 50;
 
+/*Struct of Points that have an x and y coordinate*/
 struct Point {
     double x, y;
     int index;
 };
 
+/*Create an array of Points of size N*/
 Point P[N];
 
 
-
+/*Basic function to calculate the distance of a side using Pythagorean Theorem*/
 double dist(int a, int b) {
     double dx = P[a].x - P[b].x;
     double dy = P[a].y - P[b].y;
@@ -36,6 +38,7 @@ double dist(int a, int b) {
 }
 
 
+/*Calculate the total length of the tour*/
 double tour_length(int* T) {
     double total = 0.0;
     for (int i = 0; i < N; i++) {
@@ -46,7 +49,7 @@ double tour_length(int* T) {
     return total;
 }
 
-
+/*Shuffles the contents of or points in the tour array*/
 void make_rand(int* T) {
     for (int i = 0; i < N; i++) {
         T[i] = i;
@@ -54,25 +57,34 @@ void make_rand(int* T) {
     }
 }
 
-
+/*Swaps the values in the array at a position and a randomly-chosen preceding position*/
 void substitute(int* T, int a, int b) {
     //Base Case
     if (a >= b) {return;}
 
     swap(T[a], T[b]);
+    //Recursively swap the rest of the contents of the array
     substitute(T, a + 1, b - 1);
 }
 
-
+/* Refines the tour to find the optimal-local tour*/
 bool refine_tour(int* T) {
+    //Store the original length of the tour//
     double og_len = tour_length(T);
 
+    //Loop through the entire array//
     for (int i = 0; i < N; i++) {
+        //Loop through the array to get positions i + 2//
         for (int j = i + 2; j < N; j++) {
+            //Call substitute to swap the positions accordingly//
             substitute(T, i + 1, j);
+
+            /*If the original length is greater than the current tour's length
+              then the tour has been refined*/
             if (tour_length(T) < og_len) {
                 return true;
             }
+            /*Otherwise keep refining*/
             substitute(T, i + 1, j);
         }
     }
@@ -113,11 +125,12 @@ int main (void) {
 
     cout << "Length of the most optimal tour: " << best_num << endl;
     cout << "~~~~~~~~~~~~~~~~~~~Path~~~~~~~~~~~~~~~~~~~" << endl;
+    cout << endl;
 
     for (int i = 0; i < N; i++) {
         cout << best_tour[i] << " ";
     }
-    cout << endl;
+    cout << endl << endl;
 
 return 0;
 }
