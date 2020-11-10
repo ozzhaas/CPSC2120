d15c10ab858d84254a08381a43acfcc9df222e57
ozzhaas <kellenhaas@outlook.com>
2020-11-10 15:37:15 -0500

    11.10 3:37pm

--- hw03/main.cpp
+++ hw03/main.cpp
@@ -24,8 +24,7 @@ struct Candy {
 };
 
 const int N = 16;
-const int T = 1000;
-vector<Candy> candies;
+Candy candies[N];
 int maxWeight = 2000;
 
 struct Bag {
@@ -40,7 +39,6 @@ int max (int a, int b) {
     return (a > b) ? a : b;
 }
 
-
 void sort_tasty() {
     int i, j, min;
     for (i = 0; i < N - 1; i++) {
@@ -54,10 +52,34 @@ void sort_tasty() {
     }
 }
 
+// void greedy() {
+//
+// }
 
-void greedy() {
+
+int main (void) {
+    ifstream input;
+    input.open("candy.txt");
+    string line;
+    string valString;
+    string weightString;
+    int tempVar = 0;
+    int i = 0;
     bool stored_candies[N];
     Bag bag1, bag2, bag3;
+
+
+    while (input >> tempVar) {
+        Candy tempCandy;
+        tempCandy.weight = tempVar;
+        input >> tempVar;
+        tempCandy.value = tempVar;
+        candies[i] = tempCandy;
+        i++;
+    }
+
+    sort_tasty();
+
     for (int i = N - 1; i >= 0; i--) {
         if (stored_candies[i] != true) {
             if (bag1.weight + candies[i].weight < 2000) {break;}
@@ -107,31 +129,7 @@ void greedy() {
     cout << endl;
 
     cout << "Greedy: " << bag1.totalVal + bag2.totalVal + bag3.totalVal << endl;
-}
 
-// void refined() {
-//
-// }
-
-int main (void) {
-    ifstream input;
-    input.open("candy.txt");
-    string line;
-    string valString;
-    string weightString;
-    int tempVar = 0;
-
-    while (input >> tempVar) {
-        Candy tempCandy;
-        tempCandy.weight = tempVar;
-        input >> tempVar;
-        tempCandy.value = tempVar;
-        candies.push_back(tempCandy);
-    }
-
-    sort_tasty();
-
-    greedy();
 
     return 0;
 }
