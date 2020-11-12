364fe0ba4a5afeda8c176437ea837c133dbf479f
ozzhaas <kellenhaas@outlook.com>
2020-11-11 23:33:13 -0500

    11.11 11:33pm

--- hw03/main.cpp
+++ hw03/main.cpp
@@ -172,6 +172,8 @@ int refined(vector<Bag> &bags) {
                     if (currYum > newYum) {
                         total = currYum;
                         refine_me = true;
+                        bags[0].candies_in_bag.push_back(candies[k]);
+                        remove_from_bag(bags[j], k);
                         stored_candies[k] = false;
                     }
                     else {
