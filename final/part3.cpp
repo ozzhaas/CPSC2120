0a7446be50bfedbf59e4a1e7a5a09771f70e1f82
ozzhaas <kellenhaas@outlook.com>
2020-12-11 11:39:49 -0500

    adsf

--- final/part3.cpp
+++ final/part3.cpp
@@ -6,73 +6,51 @@
    4.
 */
 
-
-
-
-
-
-#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <vector>
+#include <string>
+#include <sstream>
 #include <fstream>
+#include <iostream>
+#include <cstring>
+#include <algorithm>
 using namespace std;
 
-struct Node {
-  string myInts;
-  int freq = 0;
-  Node *next;
-  Node (string m, int f, Node *n) { myInts = m; freq = f; next = n; }
-};
 
-const int size = 1000000;
 
-int myhash(string m, int table_size)
-{
-  unsigned int i, h = 0;
-  for (i=0; i<m.length(); i++)
-    h = (h * 2917 + (unsigned int)m[i]) % table_size;
-  return h;
+struct intFreq {
+    int freq = 0;
+    int value = 0;
 }
+vector<intFreq> myInts;
 
-void count_occurrences(Node *head, string m)
-{
-  int count = 0;
-  for (Node *n = head; n != NULL; n = n->next)
-    if (n->myInts == m) {
-        count++;
-        n->freq = count;
-    }
-}
-
-// Insert to keep list sorted
-Node *insert_sorted(Node *head, int freq)
-{
-  // Base case: inserting new first element
-  if (head == NULL || freq < head->freq) return new Node(freq, head);
-  head->next = insert_sorted(head->next, freq);
-  return head;
+int compareFreq(intFreq &a, intFreq &b) {
+    return (a.freq > b.freq);
 }
 
 
-void print_ints(Node *head) {
-    if (head == NULL) return;
-    print_ints(head->next);
 
-    cout << head->myInts << "\n";
-}
 
 int main(void)
 {
-  Node *head;
-  int max = 0;
-
-  for (int i=0; i<size; i++) head[i] = NULL;
-  string in;
+    intFreq testInts;
     while (cin >> in) {
-        head = insert_sorted(head, in);
-        // int h = myhash(in, size);
-        count_occurrences(head[h], in);
-        print_ints(head);
+        testInts.value = stoi(in);
+        testInts.freq = 0;
+
+        for (unsigned int i = 0; i < myInts.size(); i++) {
+            if (testInts.value == myInts[i].value) {
+                myInts[i].freq++;
+            }
+        }
+        sort(myInts.begin(), myInts.end(), compareFreq);
     }
 
+    for (unsigned int i = 0; i < myInts.size(); i++) {
+        cout << myInts[i].value;
+    }
 
+    
   return 0;
 }
