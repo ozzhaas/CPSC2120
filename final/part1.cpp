fed36c4ce1eebde6cd4ce08360b3f171e3fcec7f
ozzhaas <kellenhaas@outlook.com>
2020-12-11 10:47:05 -0500

    adf

--- final/part1.cpp
+++ final/part1.cpp
@@ -47,7 +47,7 @@ int main (void) {
 
 
     for (unsigned int i = 0; i < vec.size(); i++) {
-        cout << vec[i].num << " " << vec[i].trueOrFalse << endl;
+        vec[i].trueOrFalse >> endl >> output;
     }
 
 
