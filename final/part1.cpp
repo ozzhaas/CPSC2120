1cf042aeba84994e7d39bea970d761e4d6015997
ozzhaas <kellenhaas@outlook.com>
2020-12-11 09:23:50 -0500

    adf

--- final/part1.cpp
+++ final/part1.cpp
@@ -21,17 +21,11 @@ int main (void) {
         string sub = temp.substr(31, ' ');
         size_t num = sub.find(' ');
         string number = sub.substr(0, num);
-        size_t is = sub.find("is");
-        string garbage = temp.substr(30, is);
-        for (int i = 0; i < sizeof(garbage); i++) {
-            vec.push_back(i);
-        }
-        cout << number << " ";
-        cout << vec[vec.size()] << endl;
-        // cout << end << endl;
+
 
     }
 
+    input.seekg(ios::beg);
 
 
 
